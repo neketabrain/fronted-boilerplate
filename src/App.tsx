001af1c193d73a0fa33acaaf2fdc0d6c82646687
Nikita Karapuzov <karapuzov.nikita@gmail.com>
2020-04-06 14:56:17 +0700

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import logo from "@assets/webpack.png";
+import logo from "@assets/react.png";
 
 import "./App.scss";
 
 const App: React.FC = () => (
   <div className="container">
-    <h1 className="title">Frontend boilerplate</h1>
+    <h1 className="title">React app boilerplate</h1>
     <img className="logo" alt="webpack_logo" src={logo} />
   </div>
 );
